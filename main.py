4c3fe97a9347af3b6a57d8ef6e2e2d0058f498c5
Sinchana B K <sinchanabk13@gmail.com>
2026-04-13 02:28:30 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-import sys
-import os
-sys.path.append(os.path.dirname(os.path.abspath(__file__)))from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from database import engine, Base
 from routers import auth_router, assets_router, users_router, dashboard_router
