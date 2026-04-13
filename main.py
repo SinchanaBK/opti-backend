843373d89eb837c119ee8254250696d2ad83d1d5
Sinchana B K <sinchanabk13@gmail.com>
2026-04-13 06:51:50 -0400

    Update main.py

--- main.py
+++ main.py
@@ -54,10 +54,10 @@ def auto_seed():
         db.commit()
 
         users_data = [
-            ("Opti Admin",    "admin@gmail.com",  "admin123",  admin_role.id),
-            ("John", "john@gmail.com",  "john123",  emp_role.id),
-            ("Ram",  "Ram@gmail.com",    "ram12",    emp_role.id),
-            ("Akash",   "akash@gmail.com",  "akash123",  emp_role.id),
+            ("Opti Admin",    "admin@opti.com",  "admin123",  admin_role.id),
+            ("John", "john@opti.com",  "john123",  emp_role.id),
+            ("Ram",  "Ram@opti.com",    "ram12",    emp_role.id),
+            ("Akash",   "akash@opti.com",  "akash123",  emp_role.id),
         ]
         user_map = {}
         for full_name, email, pwd, role_id in users_data:
